ca6cd32f239221e51cdb0791745fd5f511517a59
Ben <ben@Ben-RoG-Laptop>
2023-08-30 21:14:49 +0100

    Implement solution

---
+++ code/01-starting-setup/src/components/Concept.js
@@ -0,0 +1,11 @@
+function Concept({src, alt, title, desc}) {
+    return (
+    <li className="concept">
+        <img src={src} alt={alt} />
+            <h2>{title}</h2>
+        <p>{desc}</p>
+    </li>
+    );
+}
+
+export default Concept;
---
+++ code/01-starting-setup/src/components/Concepts.js
@@ -0,0 +1,13 @@
+import Concept from './Concept';
+
+function Concepts(props) {
+    return (
+    <ul id="concepts">
+        {props.concepts.map((concept) => (
+            <Concept key={concept.title} src={concept.image} alt={concept.alt} title={concept.title} desc={concept.desc}/>
+        ))}
+    </ul>
+    );
+}
+
+export default Concepts;
